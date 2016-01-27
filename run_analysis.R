6eacd24c06dace81b84b7edebd8014c028d9b4b8
Sonso Nuñez <lola.banderass@gmail.com>
2016-01-27 21:07:37 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -13,3 +13,23 @@ S <- rbind(temp1, temp2)
 temp1 <- read.table("UCI HAR Dataset/train/y_train.txt")
 temp2 <- read.table("UCI HAR Dataset/test/y_test.txt")
 Y <- rbind(temp1, temp2)
+
+######################################################################
+#2.	Extracts only the measurements on the mean and standard deviation for each measurement. 
+######################################################################
+
+
+
+######################################################################
+#3.	Uses descriptive activity names to name the activities in the data set
+######################################################################
+
+
+######################################################################
+#4.	Appropriately labels the data set with descriptive variable names. 
+######################################################################
+
+
+######################################################################
+#5.	From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activ
+######################################################################
