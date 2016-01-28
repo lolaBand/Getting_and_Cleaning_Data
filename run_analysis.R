74f2838bac9598f35ac30794fdaaf3b8ff206425
Sonso Nuñez <lola.banderass@gmail.com>
2016-01-28 21:12:47 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -45,7 +45,12 @@ names(Y) <- "activities"
 ######################################################################
 #4.	Appropriately labels the data set with descriptive variable names. 
 ######################################################################
-
+# set label for the unique column
+names(S) <- "subject"
+#print(head(S))
+# fussion all data set -> so crazy in loveeeee
+fussion <- cbind(S, Y, X)
+#print(head(fussion))
 
 ######################################################################
 #5.	From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activ
